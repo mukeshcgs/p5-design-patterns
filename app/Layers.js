6a1adc0134b5326849ad036c9d869cd103dfe55f
Mukesh <mukeshcgs@gmail.com>
2019-04-13 23:01:55 +0530

    tron effect

---
+++ app/Layers.js
@@ -0,0 +1,45 @@
+import p5 from "p5";
+c =
+    class Layers {
+        constructor() {
+            const CRYSTAL_SIZE = 400;
+            const SIDES = 6
+            let PALLETTE = [
+                p5.color(255, 52, 154),
+                p5.color(4, 0, 152)
+            ]
+
+            this.sides = SIDES;
+            this.numShapes = this.sides
+            this.angle = p5.TOW_PI / this.numShapes
+            this.stepOut = 8
+            // this.singleStep(CRYSTAL_SIZE / 2) / this.stepOut
+            this.thinStrok = 1
+            this.thickStrok = 3
+            this.strokeColor = this.getRandomFromPallate()
+
+        }
+        getRandomFromPallate() {
+            const rando2 = p5.floor(p5.random(0, PALLETTE.length))
+            return PALLETTE[rando2]
+        }
+    }
+export class Circle extends Layers {
+    constructor() {
+        super()
+        this.shapeSize = (CRYSTAL_SIZE / 2) * 0.93
+        this.position = (CRYSTAL_SIZE / 2) - (this.shapeSize / 2)
+    }
+    rendar() {
+        p5.noFill()
+        p5.stroke(this.strokeColor)
+        p5.strokeWeight(1)
+        p5.push()
+        p5.translate(canvasWidth / 2, canvasHeight / 2);
+        for (let i = 0; i < this.numbShapes; i++) {
+            p5.ellipse(this.position, 0, this.shapeSize, this.shapeSize)
+            p5.rotate(this.angle)
+        }
+        p5.pop()
+    }
+}
--- app/index.js
+++ app/index.js
@@ -7,7 +7,9 @@ import "styles/index.scss";
 import p5 from "p5";
 import dat from "dat.gui";
 import Stats from "stats-js";
+import { hexagon, hexagonLine } from './helper'
 
+// import { Layers, Circle } from './Layers'
 // ================================
 // START YOUR APP HERE
 // ================================
@@ -32,7 +34,6 @@ const sketch = p5 => {
   // Variables scoped within p5
   const canvasWidth = p5.windowWidth;
   const canvasHeight = p5.windowHeight;
-  // const d = new Star(500, 300, 4);
 
   // make library globally available
   window.p5 = p5;
@@ -41,8 +42,8 @@ const sketch = p5 => {
   const CRYSTAL_SIZE = 400;
   const SIDES = 6
   let PALLETTE = [
-    p5.color(255,52,154),
-    p5.color(4,0,152)
+    p5.color(255, 52, 154),
+    p5.color(4, 0, 152)
   ]
 
   // Setup function
@@ -57,28 +58,121 @@ const sketch = p5 => {
   // Draw function
   // ======================================
   p5.draw = () => {
+    //testLines()
+    // outlineShape()
+    // simpleLines()
+    tron()
+    // const layer = new Circle()
+    // layer.render()
+  }
+  //Circle
+  function circle() { }
+
+  //Helper Random 1 or 2 for Spoke count
+  function randomSelectTwo() {
+    const rando = p5.random(1)
+    if (rando > 0.5) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  //Helper Random Color
+  function getRandomFromPallate() {
+    const rando2 = p5.floor(p5.random(0, PALLETTE.length))
+    return PALLETTE[rando2]
+  }
+
+  //Simple Lines
+  function simpleLines() {
+    const stepsOut = 8
+    const numSteps = randomSelectTwo ? stepsOut : stepsOut * 1.25
+    const step = (CRYSTAL_SIZE / 2) / numSteps
+    const start = p5.floor(p5.random(0, numSteps))
+    const stop = p5.floor(p5.random(start, numSteps + 1))
+
+    let numbShapes = randomSelectTwo() ? SIDES : SIDES * 2;
+    const strokeColor = getRandomFromPallate();
+    const weight = randomSelectTwo() ? 1 : 3
+
     p5.noFill()
-    // p5.stroke(0)
-    // p5.background("#000");
+    p5.stroke(strokeColor)
+    p5.strokeWeight(weight)
 
     p5.push()
-    p5.translate(canvasWidth/2,canvasHeight/2);
-    p5.stroke(PALLETTE[1])
-    p5.ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE)
-    p5.stroke(PALLETTE[0])
-    
-    for (let i = 0; i < SIDES; i++) {
-      p5.line(0, 0, 0, CRYSTAL_SIZE )
-      // let c = 360 / SIDES;
-      // console.log(c)
-      p5.rotate(360 / SIDES)
+    p5.translate(canvasWidth / 2, canvasHeight / 2);
+    for (let i = 0; i < numbShapes; i++) {
+      p5.line(start * step, 0, stop * step, 0)
+      p5.rotate(p5.TWO_PI / numbShapes)
     }
     p5.pop()
-    
+  }
 
+  //Outside Circle
+  function outlineShape() {
+    const strokeColor = getRandomFromPallate()
+    const weight = randomSelectTwo() ? 1 : 3
+    const isHexagon = randomSelectTwo()
+    p5.stroke(strokeColor)
+    p5.strokeWeight(weight)
+
+    p5.push()
+    p5.translate(canvasWidth / 2, canvasHeight / 2);
+    if (isHexagon) {
+      hexagon(0, 0, CRYSTAL_SIZE / 2)
+    } else {
+      p5.ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE)
+    }
+    p5.pop()
+  }
+
+  // Spokes
+  function testLines() {
+    let numbShapes = randomSelectTwo() ? SIDES : SIDES * 2;
+    const strokeColor = getRandomFromPallate();
+
+    p5.noFill()
+    p5.stroke(PALLETTE[0])
+    p5.strokeWeight(1)
+
+    p5.push()
+    p5.translate(canvasWidth / 2, canvasHeight / 2);
+    p5.ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE)
+    for (let i = 0; i < numbShapes; i++) {
+      p5.line(0, 0, 0, CRYSTAL_SIZE / 2)
+      p5.stroke(255, 52, 154)
+      p5.rotate(p5.TWO_PI / numbShapes)
+    }
+    p5.pop()
+  };
+
+  // Trone Effect - 1
+  function tron() {
+    let numbShapes = randomSelectTwo() ? SIDES : SIDES;
+    const strokeColor = getRandomFromPallate();
+    p5.noFill()
+    p5.stroke(strokeColor)
+    p5.strokeWeight(0.5)
+
+    p5.push()
+    p5.translate(canvasWidth / 2, canvasHeight / 2);
+
+    for (let i = 0; i < numbShapes; i++) {
+      p5.line(0, 0, CRYSTAL_SIZE * 2, 0)
+      p5.rotate(p5.TWO_PI / numbShapes)
+
+      for (let i = 0; i < numbShapes * 2; i++) {
+        p5.stroke(getRandomFromPallate())
+        p5.strokeWeight(0.5)
+        hexagonLine(0, 0, p5.floor(p5.random(1, CRYSTAL_SIZE)))
+        // p5.ellipse(0, 0, p5.floor(p5.random(0, CRYSTAL_SIZE)), CRYSTAL_SIZE)
+      }
+
+    }
+    p5.pop()
   };
 };
-
 new p5(sketch);
 
 export default sketch;
