0e5113354d935e45d566af8a2f168a0f85fb4c04
DevM <dev.mukeshcgs@gmail.com>
2025-02-22 15:19:03 +0530

    browser sync plugin added

--- vite-app-WEBGL/vite.config.js
+++ vite-app-WEBGL/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import glsl from 'vite-plugin-glsl';
+import VitePluginBrowserSync from 'vite-plugin-browser-sync'
 
 export default defineConfig({
-  plugins: [glsl()],
+  plugins: [glsl(), VitePluginBrowserSync()],
 });
